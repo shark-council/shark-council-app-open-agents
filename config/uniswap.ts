aeb7d4ff38490aa782ff30504c730ba083526eea
kiv1n <aloha@kiv1n.ru>
2026-05-01 12:55:54 +0300

    Add missing decimals for USDC and cbBTC tokens in Uniswap configuration

--- config/uniswap.ts
+++ config/uniswap.ts
@@ -6,10 +6,12 @@ export const uniswapConfig = {
   tokens: {
     usdc: {
       symbol: "USDC",
+      decimals: 6,
       address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
     },
     btc: {
       symbol: "cbBTC",
+      decimals: 8,
       address: "0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf",
     },
   },
